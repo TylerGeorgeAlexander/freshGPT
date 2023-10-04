c6f512e2c3980d0b996258659581758f6a8a124e
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-10-04 18:01:14 -0400

    feat: "Add 'synthesize_info' ability to create reports on company responses to challenges and trends"
    Description:
    - Added a new ability named 'synthesize_info' that creates a brief report or summary.
    - This ability takes two file paths as input: 'file_path1' and 'file_path2'.
    - It reads data from these files and generates a report that highlights how one or more companies from the file specified in 'file_path1' are addressing or capitalizing on challenges or trends from the file specified in 'file_path2'.
    - The generated report is written to a file called 'output.txt'.
    - Utilizes the OpenAI GPT-3.5-turbo model to assist in generating the report content based on provided rules.
    - Created an artifact 'output.txt' in the workspace for the generated report.

--- autogpts/testgpt/forge/sdk/abilities/file_system/files.py
+++ autogpts/testgpt/forge/sdk/abilities/file_system/files.py
@@ -2,6 +2,12 @@ from typing import List
 
 from ..registry import ability
 
+# synthesize deps
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @ability(
     name="list_files",
@@ -85,3 +91,64 @@ async def read_file(agent, task_id: str, file_path: str) -> bytes:
     )
     print("Testing",file_path)
     return read_text
+
+
+# {
+#   "name": "Create a brief report or summary highlighting how one or more companies from companies.txt are addressing or capitalizing on challenges or trends from challenges.txt. Write a file called output.txt.",
+#   "input": "Create a brief report or summary highlighting how one or more companies from companies.txt are addressing or capitalizing on challenges or trends from challenges.txt. Write a file called output.txt.",
+#   "additional_input": {},
+#   "created_at": "2023-10-04T19:20:43.257265",
+#   "modified_at": "2023-10-04T19:20:43.257268",
+#   "task_id": "f7e7b984-c794-402c-9237-4f3194bde24a",
+#   "step_id": "3cf31b8e-6b9a-4030-b4c7-d131ddcc4ac8",
+#   "status": "created",
+#   "output": null,
+#   "additional_output": {},
+#   "artifacts": [],
+#   "is_last": true
+# }
+
+# synthesize_info
+
+@ability(
+    name="synthesize_info",
+    description="Create a brief report or summary highlighting how one or more companies from companies.txt are addressing or capitalizing on challenges or trends from challenges.txt. Write a file called output.txt.",
+    parameters=[
+        {
+            "name": "file_path1",
+            "description": "1st Path to the file",
+            "type": "string",
+            "required": True,
+        },
+        {
+            "name": "file_path2",
+            "description": "2nd Path to the file",
+            "type": "string",
+            "required": True,
+        },
+    ],
+    output_type="bytes",
+)
+async def synthesize_info(agent, task_id: str, file_path1: str, file_path2: str) -> bytes:
+    """
+    Read data from a file
+    """
+    # Read file 1
+    read_text1 = agent.workspace.read(task_id=task_id, path=file_path1)
+    # Read file 2
+    read_text2 = agent.workspace.read(task_id=task_id, path=file_path2)
+    # Rules
+    rules = f'Create a brief report or summary highlighting how one or more companies from {read_text1} are addressing or capitalizing on challenges or trends from {read_text2}. The company mentioned in the output must actively address or capitalize on the challenges or trends listed. Format ability like '
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": f"{rules}"}])
+    print("Testing completion", completion)
+
+    agent.workspace.write(task_id=task_id, path="/output.txt", data=completion['choices'][0]['message']['content'].encode())
+    await agent.db.create_artifact(
+        task_id=task_id,
+        file_name='output.txt',
+        relative_path='/output.txt',
+        agent_created=True,
+    )
+    return completion['choices'][0]['message']['content'].encode()
